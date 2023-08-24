e9f5edfae5b6c9ffbf73c00b3c0b91e15f3867f6
gabrielmzl <mzlcs2k@gmail.com>
2023-08-24 12:33:48 -0300

    aaa

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -2,24 +2,51 @@ import express, { Response, Request } from "express";
 
 var app = express();
 
-const users = [
-  { id: 1, name: "Lucas" },
-  { id: 2, name: "Eric" },
-  { id: 3, name: "Ana" },
-];
-
-
 app.get("/", function (req: Request, res: Response) {
-  res.send("Bohr Express template");
+  res.send("api works");
 });
 
-app.get("/users", function (req: Request, res: Response) {
-  res.send(users);
-});
+app.post("/consultar", async function (req: Request, res: Response) {
+  try {
+    const { cpf } = req.body.cpf;
 
-app.get("/users/:userId", function (req: Request, res: Response) {
-  const user = users.find((user) => user.id === parseInt(req.params.userId));
-  res.send(user);
+    const url = 'https://api.arcadiancenter.com/token/2074815d-58c9-4299-865c-fec493b6460d/CpfSimples/' + cpf
+    const { data } = await axios.post(url);
+
+    if (data.ac_service.msg === "Dados não encontrado!" || data.code === "08") {
+      const code = 0
+
+      res.status(201).json({
+        success: true,
+        consulta: code
+      });
+    } else {
+      const parsedData = {
+        code: 1,
+        nome: data.ac_service['Dados Pessoais'].Nome,
+        nome_social: data.ac_service['Dados Pessoais']['Nome Social'],
+        sexo: data.ac_service['Dados Pessoais'].Sexo,
+        conjuge: data.ac_service['Dados Pessoais'].Conjuge,
+        cpf: data.ac_service['Dados Pessoais'].CPF,
+        cpfConjuge: data.ac_service['Dados Pessoais']['CPF do Conjuge'],
+        dtSitCad: data.ac_service['Dados Pessoais']['DT Sit Cad'],
+        enderecoNascimento: data.ac_service['Dados Pessoais']['Endereco de Nascimento'],
+        racaCor: data.ac_service['Dados Pessoais']['Raca/Cor'],
+        falecido: data.ac_service['Dados Pessoais'].Falecido,
+        dataFalecimento: data.ac_service['Dados Pessoais']['Data Falecimento']
+      }
+
+      res.status(201).json({
+        success: true,
+        consulta: parsedData
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error
+    });
+  }
 });
 
 if (!module.parent) {
